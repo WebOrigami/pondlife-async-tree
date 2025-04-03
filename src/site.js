0e015f3096650d1354789500bd37e9cd214ef059
Jan Miksovsky <jan@miksovsky.com>
2025-04-02 17:48:08 -0700

    Pagination

--- src/site.js
+++ src/site.js
@@ -1,4 +1,4 @@
-import { FileTree, map } from "@weborigami/async-tree";
+import { FileTree, map, paginate } from "@weborigami/async-tree";
 import { rss } from "@weborigami/origami";
 import data from "./data.js";
 import jsonFeed from "./jsonFeed.js";
@@ -6,6 +6,14 @@ import aboutPage from "./templates/aboutPage.js";
 import multiPostPage from "./templates/multiPostPage.js";
 import singlePostPage from "./templates/singlePostPage.js";
 
+// Group posts into sets of 10
+const pages = map(await paginate(data, 10), {
+  key: (index) => `${index}.html`,
+  inverseKey: (key) => parseInt(key, 10),
+  value: multiPostPage,
+});
+
+// Convert posts to a feed object in JSON Feed schema
 const feed = await jsonFeed(data);
 
 /**
@@ -15,8 +23,9 @@ export default {
   "about.html": aboutPage(),
   assets: new FileTree(new URL("../src/assets", import.meta.url)),
   images: new FileTree(new URL("../images", import.meta.url)),
-  "index.html": multiPostPage(data),
+  "index.html": pages.get("1.html"), // same as first page in pages area
   "feed.json": JSON.stringify(feed, null, 2),
   "feed.xml": rss.call(null, feed),
+  pages,
   posts: map(data, (value, key, tree) => singlePostPage(value, key, tree)),
 };
--- src/templates/multiPostPage.js
+++ src/templates/multiPostPage.js
@@ -4,8 +4,9 @@ import page from "./page.js";
 import postFragment from "./postFragment.js";
 
 // A page showing multiple posts
-export default async (posts) => {
-  const postFragmentsTree = await map(posts, postFragment);
+export default async (paginated) => {
+  const { items, nextPage, previousPage } = paginated;
+  const postFragmentsTree = await map(items, postFragment);
   const postFragments = await Tree.values(postFragmentsTree);
   return page({
     title: siteInfo.title,
@@ -14,10 +15,29 @@ export default async (posts) => {
       <h1>${siteInfo.title}</h1>
       <p>${siteInfo.description}</p>
       ${postFragments.join("\n")}
-      <footer>
-        <a href="/feed.xml">RSS feed</a>
-        <a href="/feed.json">JSON feed</a>
-        <a href="https://github.com/WebOrigami/origami-blog-start">View source</a>
+      <p>
+        ${
+          nextPage
+            ? `
+              <a class="next" href="/pages/${nextPage}.html"><strong>Older posts</strong></a>
+              <span>&nbsp;</span>
+            `
+            : ""
+        }
+        ${
+          previousPage
+            ? `
+              <a class="previous" href="${
+                previousPage == 1 ? "/" : `/pages/${previousPage}.html`
+              }"><strong>Newer posts</strong></a>
+            `
+            : ""
+        }
+       </p>
+       <footer>
+       <a href="/feed.xml">RSS feed</a>
+       <a href="/feed.json">JSON feed</a>
+       <a href="https://github.com/WebOrigami/origami-blog-start">View source</a>
       </footer>
     `,
   });
