00cafe8bd6206f5e8a7fa8204a493ef3e13ebe7c
Jan Miksovsky <jan@miksovsky.com>
2025-10-27 15:09:50 -0700

    More with FileMap

--- src/build.js
+++ src/build.js
@@ -1,7 +1,7 @@
-import { FileTree, Tree } from "@weborigami/async-tree";
+import { FileMap, Tree } from "@weborigami/async-tree";
 import site from "./site.js";
 
 // Build process writes the site resources to the build folder
-const buildTree = new FileTree(new URL("../build", import.meta.url).pathname);
+const buildTree = new FileMap(new URL("../build", import.meta.url).pathname);
 await Tree.clear(buildTree); // Erase any existing files
 await Tree.assign(buildTree, site); // Copy site to build folder
--- src/templates/aboutPage.js
+++ src/templates/aboutPage.js
@@ -1,14 +1,14 @@
 import { marked } from "marked";
-import fs from "node:fs/promises";
+import fs from "node:fs";
 import markdownDocument from "../markdownDocument.js";
 import page from "./page.js";
 
 // About page: transforms about.md to HTML and applies the page template
-export default async () => {
-  const buffer = await fs.readFile(
+export default () => {
+  const buffer = fs.readFileSync(
     new URL("../about.md", import.meta.url).pathname
   );
-  const document = await markdownDocument(buffer);
+  const document = markdownDocument(buffer);
   return page({
     ...document,
     // Transform the body to HTML
